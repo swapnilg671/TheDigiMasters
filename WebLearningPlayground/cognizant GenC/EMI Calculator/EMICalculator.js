9c0eeee6c8c26eeb132ee8764e833392d1ec4812
swapnilg671 <swapnilg671@gmail.com>
2021-08-14 19:57:47 +0530

    js calc

--- WebLearningPlayground/cognizant GenC/EMI Calculator/EMICalculator.js
+++ WebLearningPlayground/cognizant GenC/EMI Calculator/EMICalculator.js
@@ -5,10 +5,11 @@ const R=interestRate/(100*12);
 
 
 function calculateEMI(P,N, R){
-let E=P*R*(Math.pow(1+R,N)/(Math.pow(1+R,N)-1));
-let EMI=E.toFixed(2);
+let E=P*R*(Math.pow(1+R,N)/(Math.pow(1+R,N)-1));//Math.pow for exponent
+
+let EMI=E.toFixed(2);//  fix the decimal point to 2 digit 
 document.getElementById("result").innerHTML="EMI is Rs."+ EMI;
 
-return false;
+return false;// prevent fuction to propogate fast 
 };
 
